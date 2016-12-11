8c72865cb9ff140e679f083b36b5d516e2df822e
Ryan <Hall.Ryan.R@gmail.com>
2016-12-11 12:10:36 -0800

    change last migration to null defaule

--- db/migrate/20161211190257_add_rebuttals.rb
+++ db/migrate/20161211190257_add_rebuttals.rb
@@ -1,7 +1,7 @@
 class AddRebuttals < ActiveRecord::Migration[5.0]
   def change
-    add_column :blacklists, :rebuttal_id, :integer
-    add_column :concepts, :rebuttal_id, :integer
+    add_column :blacklists, :rebuttal_id, :integer, null: true
+    add_column :concepts, :rebuttal_id, :integer, null: true
     add_index :blacklists, :rebuttal_id
     add_index :concepts, :rebuttal_id
   end
